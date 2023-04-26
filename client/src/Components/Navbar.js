3c1e96237f7bab196e540abd971d9d99664e8804
Alice Parrish <alice@pop-os.localdomain>
2023-04-26 19:48:01 -0400

    Created very basic landing page

---
+++ client/src/Components/Navbar.js
@@ -0,0 +1,15 @@
+import './Navbar.css'
+
+function Navbar() {
+
+    return (
+        <div id="navbar">
+            <h1>ABCL Recipe Book</h1>
+            <h2>Search Bar</h2>
+            <h2>Login/Signup</h2>
+        </div>
+    )
+
+}
+
+export default Navbar
