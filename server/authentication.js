9cf37300777ab505906e21cb46a232d6211743d7
Alice Parrish <alice@pop-os.localdomain>
2023-05-01 01:48:18 -0400

    Added function for confirming tokens

--- server/authentication.js
+++ server/authentication.js
@@ -5,23 +5,27 @@ const { Op } = require('sequelize')
 const cookie = require('cookie')
 const hash = require('hash.js')
 
+function generateToken() {
+    const currentDate = new Date()
+    const timestamp = currentDate.getTime()
+    const rand = Math.random() * 1000000
+
+    return hash.sha256().update(String(timestamp + rand)).digest('hex')
+}
+
 class Authentication {
 
-    static async createToken(user_id) {
+    static async createCookie(user_id) {
 
-        const destroyedCookies = await Session_cookies.destroy({
+        await Session_cookies.destroy({
             where: {
                 user_id: user_id
             }
         })
 
-        const currentDate = new Date()
-        const timestamp = currentDate.getTime()
-        const rand = Math.random() * 1000000
-
-        const sessionTokenInfo = {
+        const sessionCookieInfo = {
             user_id: user_id,
-            session_token: hash.sha256().update(String(timestamp + rand)).digest('hex')
+            session_token: generateToken()
         }
 
         const sessionToken = await Session_cookies.create(sessionTokenInfo)
