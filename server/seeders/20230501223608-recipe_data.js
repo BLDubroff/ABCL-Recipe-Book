a0e3ab4d9428090c836ec048a2b5cdf1cf968810
Ben Dubroff <BLDubroff@gmail.com>
2023-05-01 18:52:15 -0400

    fixed recipe_data seeder

--- server/seeders/20230501223608-recipe_data.js
+++ server/seeders/20230501223608-recipe_data.js
@@ -7,6 +7,7 @@ module.exports = {
       recipe_id: 1 ,
       user_id: 1 ,
       title: 'Focaccia Bread',
+      pic: null,
       description: 'Delicious flaky Italian bread',
       recipe_content: 'make your grandmother proud and bake bread',
       prep_time_in_minutes: 180,
@@ -17,7 +18,8 @@ module.exports = {
         'Italian',
         'Dinner',
         'Snack'
-      ]
+      ],
+      avg_rating: null
     }], {});
   },
 
