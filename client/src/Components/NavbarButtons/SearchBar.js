edad7e56f22d3546251d16722e3a25acfbbdc38e
Alice Parrish <alice@pop-os.localdomain>
2023-05-03 19:31:38 -0400

    Added search page

--- client/src/Components/NavbarButtons/SearchBar.js
+++ client/src/Components/NavbarButtons/SearchBar.js
@@ -3,10 +3,13 @@ import './Navbarbuttons.css'
 import Form from 'react-bootstrap/Form';
 import Button from 'react-bootstrap/Button'
 import ServerContext from '../../Features/ServerContext';
+import { useNavigate } from 'react-router-dom';
 
 export default function SearchBar() {
 
-  const searchTerm = useRef('');
+  const navigate = useNavigate()
+
+  const searchTerm = useRef('')
 
   const { serverURL } = useContext(ServerContext)
 
@@ -14,22 +17,25 @@ export default function SearchBar() {
 
     e.preventDefault()
 
-    fetch(`${serverURL}/recipes/search/?tags=${await JSON.stringify(tags.split(' '))}`, {
-      method: 'GET',
-      mode: 'cors',
-      headers: {
-          "Content-Type": "application/json",
-      },
-  })
-      .then(res => res.json())
-      .then(body => {
-        console.log(body)
-      })
+    // fetch(`${serverURL}/recipes/search/?tags=${await JSON.stringify(tags.split(' '))}`, {
+    //   method: 'GET',
+    //   mode: 'cors',
+    //   headers: {
+    //       "Content-Type": "application/json",
+    //   },
+    // })
+    //   .then(res => res.json())
+    //   .then(body => {
+    //     console.log(body)
+    //   })
+    
+    navigate(`/search/?tags=${await JSON.stringify(tags.split(' '))}`)
+    
   }
 
   return (
     <div>
-         <Form className="d-flex">
+         <Form className="d-flex" onSubmit={(e) => handleSearch(e, searchTerm.current.value)}>
             <Form.Control
               ref={searchTerm}
               type="search"
